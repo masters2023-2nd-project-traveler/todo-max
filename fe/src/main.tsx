da811e16d5d9ed931bf15731cbafabe76632ba50
WK <mwk0725@gmail.com>
2023-07-11 15:24:56 +0900

    chore: msw 설정

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import GlobalStyles from './styles/GlobalStyles.ts';
+import worker from './mocks/worker.ts';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
