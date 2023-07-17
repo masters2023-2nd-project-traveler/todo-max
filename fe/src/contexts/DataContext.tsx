ac17532da53c7ebe3c359ccdc7389e3b59ca0d54
JH <vvaa277@gmail.com>
2023-07-17 16:25:57 +0900

    [FE] feat: context파일 타입 추가 (#69)

--- fe/src/contexts/DataContext.tsx
+++ fe/src/contexts/DataContext.tsx
@@ -1,8 +1,21 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 
+type TaskType = {
+  taskId: number;
+  title: string;
+  contents: string;
+  platform: string;
+};
+
+type TodoItemType = {
+  processId: number;
+  name: string;
+  tasks: TaskType[];
+};
+
 type DataType = {
-  todoListData: any;
-  setTodoListData: (value: any) => void;
+  todoListData: TodoItemType[] | null;
+  setTodoListData: (value: TodoItemType[] | null) => void;
 };
 
 const DataContext = createContext<DataType>({
@@ -15,7 +28,7 @@ type DataProviderProps = {
 };
 
 export const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
-  const [todoListData, setTodoListData] = useState(null);
+  const [todoListData, setTodoListData] = useState<TodoItemType[] | null>(null);
 
   return (
     <DataContext.Provider value={{ todoListData, setTodoListData }}>
